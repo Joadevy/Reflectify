35f4847662b16a9eeb6aaa5fe097f5381e6b50c0
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-08-22 02:02:32 -0300

    feat: add todo form & post to api endpoint

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,15 @@ const PORT = process.env.PORT || 5000
 const app = express()
 // app.use(api)
 
-app.use('/api', (req, res) => {
-    res.json({ message: 'dear user, I\'m the server!' });
+app.use(express.json());
+
+// app.get('/api', (req, res) => {
+//     res.json({ message: 'dear user, I\'m the server!' });
+// });
+
+app.post('/api', (req, res) => {
+    console.log(req.body.description)
+    res.status(201).json({ message: 'Todo created successfully`' });
 });
 
 app.listen(5000, () => console.log("Server started"))
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,21 +4,84 @@ type response = {
   message: string;
 };
 
-function App() {
-  const [message, setMessage] = useState<string>("");
+interface TodoForm extends HTMLFormElement {
+  todo: HTMLInputElement;
+}
 
-  const handleFetch = async () => {
-    const response: Promise<response> = fetch("/api").then((res) => res.json());
-    const data = await response;
-    setMessage(data.message);
+type Todo = {
+  id: number;
+  description: string;
+};
+
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleSubmit = async (event: React.FormEvent<TodoForm>) => {
+    event.preventDefault();
+    const inputValue = event.currentTarget.todo.value.trim();
+
+    if (!inputValue) return;
+
+    const todo = {
+      id: new Date().getTime(),
+      description: inputValue,
+    };
+
+    setTodos([...todos, todo]);
+    event.currentTarget.todo.value = "";
+
+    try {
+      const resp: response = await fetch("/api", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(todo),
+      }).then((res) => res.json());
+
+      console.log(resp.message);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <>
-      <div>Hello {message}</div>
-      <button onClick={handleFetch} className="border border-red-600 p-4 mt-2">
-        Request backend c:
-      </button>
+      <h1 className="text-center text-xl font-bold mt-5">
+        To-do but with backend server!
+      </h1>
+      <form
+        action=""
+        onSubmit={handleSubmit}
+        className="flex flex-row gap-2 p-4 max-w-sm m-auto"
+      >
+        <label htmlFor="todoInput">
+          <input
+            name="todo"
+            id="todoInput"
+            type="text"
+            placeholder="Create new todo..."
+            className="border border-purple-400 rounded-md p-2"
+          />
+        </label>
+
+        <button type="submit" className=" bg-purple-600 rounded-md p-2 ">
+          Create
+        </button>
+      </form>
+
+      {todos.length > 0 && (
+        <ul className="flex flex-col gap-2 p-4 mt-5  max-w-sm m-auto">
+          {todos.map((todo) => (
+            <li
+              key={todo.id}
+              className="border border-purple-400 rounded-md p-2"
+            >
+              {todo.description}
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
