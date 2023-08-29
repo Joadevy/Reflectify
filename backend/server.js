983492111e04e01b5ca19275ff4617be6d572980
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-08-28 23:43:40 -0300

    refactor: add route handlers from routes

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,25 @@
-// src/server/index.ts
 import express from "express"
-const PORT = process.env.PORT || 5000
-// import { api } from "./api"
+import 'dotenv/config';
+const PORT = process.env.PORT || 4000
+import mongoose from "mongoose";
+import newReflection from './routes/api/newReflection.js';
+import getReflections from './routes/api/getReflections.js';
+import dislikeReflection from './routes/api/dislikeReflection.js';
+import likeReflection from './routes/api/likeReflection.js';
 
-const app = express()
-// app.use(api)
+// Conecto localmente a la db (en remoto solo esta autorizada la ip de mi pc)
+mongoose.connect(process.env.MONGODB_URL).then(
+    () => console.log("Connected to MongoDB")).catch
+    ((error) => console.log(error.message)
+);
 
+const app = express();
 app.use(express.json());
 
-app.get('/api', (req, res) => {
-    // llamar a la db y traer los ultimos 20 thoughts por ej y mandarlos.
-    res.status(200).json([{ country: "Argentina", username: "Joaquin",id:"12312321213", date: "2023-08-26T20:18:48.895Z", description:"Mensaje 'guardado', test desde server!", likes:0 }]);
-});
-
-app.post('/api', (req, res) => {
-    console.log(req.body)
-    // guardar en la db el thought que le llega en el body
-    res.status(201).json({ message: 'Reflection created successfully' });
-});
-
-app.put(`/api/:thoughtId/like`, (req, res) => {
-    console.log(req.body)
-    // actualizar en la db el thought que le llega en el body, probablemente porque aumentaron los likes
-    res.status(200).json({ message: 'Reflection updated successfully' });
-});
-
-app.put(`/api/:thoughtId/dislike`, (req, res) => {
-    console.log(req.body)
-    // actualizar en la db el thought que le llega en el body, probablemente porque aumentaron los likes
-    res.status(200).json({ message: 'Reflection updated successfully' });
-});
+// Routes
+app.use('/api', newReflection);
+app.use('/api', getReflections);
+app.put(`/api/:thoughtId/like`, likeReflection);
+app.put(`/api/:thoughtId/dislike`, dislikeReflection );
 
 app.listen(5000, () => console.log("Server started"))
