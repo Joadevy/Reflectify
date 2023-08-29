6b325d94360590363c0d87b328649e5d810c8753
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-08-29 01:35:01 -0300

    feat: add reflection model

--- backend/model/Reflection.js
+++ backend/model/Reflection.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
 
-
 const ReflectionSchema = new Schema({
-    id: { type: String, required: true },
-    country: { type: String, required: true },
-    username: { type: String, required: true },
-    date: { type: Date, required: true },
-    description: { type: String, required: true },
-    likes: { type: Number, required: true },
+  id: { type: String, required: true },
+  country: { type: String, required: true },
+  username: { type: String, required: true },
+  date: { type: Date, required: true },
+  description: { type: String, required: true },
+  likes: { type: Number, required: true },
 });
 
+const Reflection = mongoose.model("Reflection", ReflectionSchema);
+
+export default Reflection;
