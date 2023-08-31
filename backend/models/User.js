c6e0c40891b9c5249aa1eed0715a150228304b84
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-08-30 22:25:17 -0300

    feat: add User model

---
+++ backend/models/User.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const UserSchema = new Schema({
+  id: { type: String, required: true },
+  country: { type: String, required: true },
+  username: { type: String, required: true },
+  registration_date: { type: Date, required: true },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
