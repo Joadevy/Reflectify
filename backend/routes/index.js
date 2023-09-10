adf4a95d51cc61cd861fed312a09cdb38a03d992
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-09-09 21:55:56 -0300

    chore: small changes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -11,6 +11,6 @@ export const indexRouter = express.Router();
 
 // falta agregar el middleware VerifyJWT cuando implemente el login en el frontend
 indexRouter.get("/", VerifyJWt, getAllReflections);
+indexRouter.post("/", VerifyJWt, createNewReflection);
 indexRouter.patch("/:reflectionId/dislike", VerifyJWt, handleDislikeReflection);
 indexRouter.patch("/:reflectionId/like", VerifyJWt, handleLikeReflection);
-indexRouter.post("/", VerifyJWt, createNewReflection);
