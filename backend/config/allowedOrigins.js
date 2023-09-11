81a8058e512ef1ad70831a1b5e73976932a9b9f6
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-09-11 00:45:37 -0300

    chore: add remote origins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,9 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
-  "https://reflections-app.vercel.app",
+  "https://reflectifyapp.vercel.app/",
+  "https://reflectify-git-main-joadevy.vercel.app/",
+  "https://reflectify-joadevy.vercel.app/",
 ];
 
 export default allowedOrigins;
